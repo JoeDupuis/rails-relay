34d5b3dd71635ec786655e4c99208bfe2bca19d9
Joé Dupuis <joe@dupuis.io>
2025-12-23 15:31:45 -0600

    fix more kamal issues

--- config/initializers/connection_health_check.rb
+++ config/initializers/connection_health_check.rb
@@ -3,4 +3,5 @@ Rails.application.config.after_initialize do
   next if ENV["SECRET_KEY_BASE_DUMMY"]
 
   ConnectionHealthCheckJob.perform_later
+rescue ActiveRecord::StatementInvalid, SolidQueue::Job::EnqueueError
 end
