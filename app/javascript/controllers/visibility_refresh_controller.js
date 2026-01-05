7575cfacb40badc6eae60c403d359f7e3f2703f4
Joé Dupuis <joe@dupuis.io>
2026-01-04 21:40:39 -0800

    Fix visibility refresh

--- app/javascript/controllers/visibility_refresh_controller.js
+++ app/javascript/controllers/visibility_refresh_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    this.boundRefresh = this.refresh.bind(this)
+    document.addEventListener("visibilitychange", this.boundRefresh)
+    this.refresh()
+  }
+
+  disconnect() {
+    document.removeEventListener("visibilitychange", this.boundRefresh)
+  }
+
+  refresh() {
     if (document.visibilityState === "visible") {
       Turbo.visit(window.location.href, { action: "replace" })
     }
