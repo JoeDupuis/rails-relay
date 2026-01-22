f0432dd4016ed2e5ec84b1259907d8ee4bd98435
Joé Dupuis <joe@dupuis.io>
2026-01-21 17:25:08 -0800

    Deactivate cloudflare scraping protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   include Authentication
   allow_browser versions: :modern
   stale_when_importmap_changes
+  before_action :disable_cloudflare_scrape_shield
+
+  private
+
+  def disable_cloudflare_scrape_shield
+    response.headers["X-CF-Data-Scraping-Protection"] = "off"
+  end
 end
